81d2978d9b1f15a128c40457052f703588fcb848
sevvalbrt <sevval.barut@hotmail.com>
2021-01-23 15:47:23 +0300

    DictionaryDemo

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] temporaryArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < temporaryArray.Length; i++)
+            {
+                items[i] = temporaryArray[i];
+            }
+            items[items.Length - 1] = item;
+            
+        }
+
+        public int Count
+        {
+            get { return items.Length; } 
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+        
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> persons = new MyDictionary<string>();
+            persons.Add("Şevval");
+            persons.Add("Ayşe");
+            persons.Add("Meral");
+
+            
+            foreach (var person in persons.Items)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("Kişi Sayısı:" + persons.Count);
+
+            persons.Add("Sümeyye");
+            persons.Add("Kadriye");
+            Console.WriteLine("\t");
+
+            foreach (var person1 in persons.Items)
+            {
+                
+                Console.WriteLine(person1);
+            }
+            Console.WriteLine("Kişi Sayısı:" + persons.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
